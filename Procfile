9d48901bd10ec9db68412ae1423af89c0e753e76
JumpyJay <iuswtric1909@gmail.com>
2024-12-14 13:57:57 +0700

    fix Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:wthrpt
+web: gunicorn app:app
--- app.py
+++ app.py
@@ -191,5 +191,6 @@ def logout():
     return redirect("/")
 
 if __name__ == '__main__':
+    print(f"Flask app name is: {app.name}")
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
